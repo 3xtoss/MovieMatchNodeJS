5c2f7378cebee4a635dc5c84798af410e1196d68
3xtoss <jaredmulhausen@gmail.com>
2018-09-16 23:26:15 -0500

    updating everything to the github

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+/*const express = require('express');
 var path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname + '/about.html'));
 });
@@ -13,3 +15,64 @@ app.get('/about', (req, res) => {
 app.listen(8000, () => {
   console.log('Example app listening on port 8000')
 });
+
+*/
+
+
+// Imports the Google Cloud client library
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+/**
+ * TODO(developer): Uncomment the following line before running the sample.
+ */
+
+const fileName = 'images.jpeg';
+// Detect similar images on the web to a local file
+client
+  .webDetection(fileName)
+  .then(results => {
+    const webDetection = results[0].webDetection;
+    console.log(Object.keys(webDetection));
+    if (webDetection.fullMatchingImages.length) {
+      console.log(
+        `Full matches found: ${webDetection.fullMatchingImages.length}`
+      );
+      webDetection.fullMatchingImages.forEach(image => {
+        console.log(`  URL: ${image.url}`);
+        console.log(`  Score: ${image.score}`);
+      });
+    }
+
+    if (webDetection.partialMatchingImages.length) {
+      console.log(
+        `Partial matches found: ${webDetection.partialMatchingImages.length}`
+      );
+      webDetection.partialMatchingImages.forEach(image => {
+        console.log(`  URL: ${image.url}`);
+        console.log(`  Score: ${image.score}`);
+      });
+    }
+
+    if (webDetection.webEntities.length) {
+      console.log(`Web entities found: ${webDetection.webEntities.length}`);
+      webDetection.webEntities.forEach(webEntity => {
+        console.log(`  Description: ${webEntity.description}`);
+        console.log(`  Score: ${webEntity.score}`);
+      });
+    }
+
+    if (webDetection.bestGuessLabels.length) {
+      console.log(
+        `Best guess labels found: ${webDetection.bestGuessLabels.length}`
+      );
+      webDetection.bestGuessLabels.forEach(label => {
+        console.log(`  Label: ${label.label}`);
+      });
+    }
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
